a47e612473bf99545735b5d8779cfcd8d64802a3
GisnetzEU <gismontero.mail@gmail.com>
2020-09-29 16:54:27 +0200

    first commit

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,13 +3,19 @@ import React from 'react';
 import './Person.css'
 
 const person = (props) => {
+    const rnd = Math.random();
+
+    if (rnd > 0.7) {
+        throw new Error('Something went wrong');
+    }
     return (
+
         <div className="Person">
             <p onClick={props.click}>I am {props.name} and I am {props.age} years </p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value= {props.name}/>
+            <input type="text" onChange={props.changed} value={props.name} />
         </div>
-        )
+    )
 };
 
 export default person;
